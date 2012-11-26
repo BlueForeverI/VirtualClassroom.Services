bb75248ff3dc4a1832df083033593f6fc8f1ba12
BlueForeverI <bluef0rever@abv.bg>
2012-11-26 21:17:43 +0200

    Added a StudentManagement service with a sample register method. Using BCrypt.Net for password hashing.

---
+++ VirtualClassroom.Services/StudentServices/IStudentManagement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using VirtualClassroom.Services.POCO_Classes;
+
+namespace VirtualClassroom.Services.StudentServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IStudentManagement" in both code and config file together.
+    [ServiceContract]
+    public interface IStudentManagement
+    {
+        [OperationContract]
+        void RegisterStudent(Student student, string password);
+    }
+}
---
+++ VirtualClassroom.Services/StudentServices/StudentManagement.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using VirtualClassroom.Services.POCO_Classes;
+using BCrypt.Net;
+
+namespace VirtualClassroom.Services.StudentServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "StudentManagement" in code, svc and config file together.
+    public class StudentManagement : IStudentManagement
+    {
+        VirtualClassroomEntities entities = new VirtualClassroomEntities();
+
+        public void RegisterStudent(Student student, string password)
+        {
+            if(IsValidStudent(student))
+            {
+                StudentEntity studentEntity = Student.ToStudentEntity(student);
+                studentEntity.PasswordHash = BCrypt.Net.BCrypt.HashPassword(password);
+                entities.StudentEntities.AddObject(studentEntity);
+                entities.SaveChanges();
+
+            }
+        }
+
+        //to implement
+        private bool IsValidStudent(Student student)
+        {
+            return true;
+        }
+    }
+}
