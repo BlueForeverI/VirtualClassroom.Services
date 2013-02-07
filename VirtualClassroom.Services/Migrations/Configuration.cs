5be9de2d05f4d03ec59f7a883f7c9a56a7170bfd
BlueForeverI <bluef0rever@abv.bg>
2013-02-07 15:31:58 +0200

    Trying to fix the last commit

--- VirtualClassroom.Services/Migrations/Configuration.cs
+++ VirtualClassroom.Services/Migrations/Configuration.cs
@@ -14,8 +14,7 @@ namespace VirtualClassroom.Services.Migrations
 
         protected override void Seed(VirtualClassroom.Services.VirtualClassroomEntities context)
         {
-            //  This method will be called after migrating to the latest version.
-
+            //  This method will be called  after migrating to the latest version.
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
