ed18df0436acbab0470c36b2d76fbbbbee025289
BlueForeverI <bluef0rever@abv.bg>
2013-02-28 08:36:55 +0200

    Set wsHttpBinding security to <message establishSecurityContext="true"/>

--- VirtualClassroom.Services/Services/IAdminService.cs
+++ VirtualClassroom.Services/Services/IAdminService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
-//using VirtualClassroom.Services.POCO_Classes;
 using System.ServiceModel.Web;
 using System.Text;
 using System.Collections.ObjectModel;
